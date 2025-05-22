f106671b5e0b5f121a028747da3ea4db78cc8a01
SolijonovAsadbek <109437399+AsadbekSolijonov@users.noreply.github.com>
2025-05-22 17:49:42 +0500

    paginate

--- pagenate_bot/pagenate.py
+++ pagenate_bot/pagenate.py
@@ -6,8 +6,9 @@ from os import getenv
 from aiogram import Bot, Dispatcher, html, F
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message, InlineKeyboardButton
+from aiogram.filters import CommandStart, Command
+from aiogram.filters.callback_data import CallbackData
+from aiogram.types import Message, InlineKeyboardButton, CallbackQuery
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from dotenv import load_dotenv
 
@@ -16,12 +17,23 @@ TOKEN = getenv("BOT_TOKEN")
 
 dp = Dispatcher()
 
+smiles = ['☹️ Upset', '🙂 Smile', '🤩 Happy', '😡 Angry']
 
-def page_keyboards():
+
+class PageCallbackData(CallbackData, prefix='page'):  # 'page:next:1' yoki 'page:prev:1'
+    action: str
+    page: int
+
+
+def page_keyboards(page: int = -1):
     builder = InlineKeyboardBuilder()
     builder.row(
-        InlineKeyboardButton(text="Avvalgi", callback_data='prev'),
-        InlineKeyboardButton(text="Keyingi", callback_data='next')
+        InlineKeyboardButton(
+            text="Avvalgi",
+            callback_data=PageCallbackData(action='prev', page=page).pack()),
+        InlineKeyboardButton(
+            text="Keyingi",
+            callback_data=PageCallbackData(action='next', page=page).pack())
     )
     return builder.as_markup()
 
@@ -29,6 +41,29 @@ def page_keyboards():
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     await message.answer(f"Salom, {html.bold(message.from_user.full_name)}!", reply_markup=page_keyboards())
+
+
+@dp.message(Command('help', prefix='/%$'))
+async def cmd_help(message: Message) -> None:
+    await message.answer('Yordam')
+
+
+# @dp.callback_query(PageCallbackData.filter(F.action == 'prev'))
+# @dp.callback_query(PageCallbackData.filter(F.action == 'next'))
+@dp.callback_query(PageCallbackData.filter())
+async def callbacks_data(call: CallbackQuery, callback_data: PageCallbackData):
+    page = int(callback_data.page)
+    if callback_data.action == 'next':
+        page += 1
+    elif callback_data.action == 'prev':
+        page += -1
+    await call.message.edit_text(text=f"{smiles[page]}\n{page} - sahifa",
+                                 reply_markup=page_keyboards(page))
+
+
+# @dp.callback_query(PageCallbackData.filter(F.action == 'prev'))
+# async def callbacks_data(call: CallbackQuery, callback_data: PageCallbackData):
+#     pass
 
 
 async def main() -> None:
